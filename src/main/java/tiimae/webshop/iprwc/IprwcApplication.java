81f9026ae7583b4439307055de16ce25f909c980
Tim de kok <de.kok.ac@gmail.com>
2022-11-21 09:07:36 +0100

    initial commit

---
+++ src/main/java/tiimae/webshop/iprwc/IprwcApplication.java
@@ -0,0 +1,13 @@
+package tiimae.webshop.iprwc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IprwcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IprwcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tiimae/webshop/iprwc/IprwcApplicationTests.java
@@ -0,0 +1,13 @@
+package tiimae.webshop.iprwc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IprwcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
