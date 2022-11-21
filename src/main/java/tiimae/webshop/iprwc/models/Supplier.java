0738e4ddbc0c104ab37a5b64cf202ba4535c0d32
Tim de kok <de.kok.ac@gmail.com>
2022-11-21 10:38:33 +0100

    feat: added the rest of the entities with id

---
+++ src/main/java/tiimae/webshop/iprwc/models/Supplier.java
@@ -0,0 +1,22 @@
+package tiimae.webshop.iprwc.models;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "\"supplier\"")
+public class Supplier {
+
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(name = "id", columnDefinition = "VARCHAR(255)")
+    private UUID id;
+
+}
